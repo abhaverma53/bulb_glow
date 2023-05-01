483e59210984e5cbea4d6fb95e3439ddcdea29c0
abha <abhaverma53@gmail.com>
2023-05-01 16:25:44 +0530

    first commit

---
+++ app/controllers/bulb_controller.rb
@@ -0,0 +1,2 @@
+class BulbController < ApplicationController
+end
---
+++ app/javascript/application.js
@@ -0,0 +1,19 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+  
+
+
+
+
+
+  
+
+ function bulb_on(){
+      document.getElementById('bulb').src='https://i.postimg.cc/6QyTynzr/bulb-on.png';
+  }
+  function bulb_off(){
+      document.getElementById('bulb').src='https://i.postimg.cc/KjK1wL3c/bulb-off.png';
+  }
+  
